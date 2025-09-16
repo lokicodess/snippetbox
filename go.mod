8e55f0e1ce4e9e6d46f4ee5ea36484f021a52a72
lokicodess <lokesh3721@gmail.com>
2025-09-16 22:10:29 +0530

    basic server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module snippetbox.lokeshsingh.dev
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+
+		// w.WriteHeader(405)
+		// w.Write([]byte("method not allowed"))
+		//
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a snippet..."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("starting server on Port 8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
