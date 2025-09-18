b6985b517ea1361c9401fbc5ac608cbd6a139c15
lokicodess <lokesh3721@gmail.com>
2025-09-18 18:00:09 +0530

    refactor code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,50 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
-	"strconv"
+	"os"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from snippetbox"))
-}
-
-func snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
-
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", "POST")
-
-		// w.WriteHeader(405)
-		// w.Write([]byte("method not allowed"))
-		//
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Write([]byte("Create a snippet..."))
-}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("starting server on Port 8080")
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
+	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	logger.Info("Starting server ", slog.String("addr", "4000"))
+
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
